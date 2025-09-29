a05d0f762acd5fd440ecb5747fcc05114bb9a5ed
THIERRY-DEV-ENG <thierrynishimwe936gmail.com>
2025-09-29 17:16:18 +0200

    first commit

---
+++ example.js
@@ -0,0 +1,17 @@
+let globalCVar = "I am a global variable";
+function exampleFunction() {
+    let localCVar = "I am a local variable";
+    if (true) {
+        let blockCVar = "I am a block scoped variable";
+        console.log(blockCVar); 
+        console.log(globalCVar);
+        console.log(localCVar);
+    
+    }
+}
+exampleFunction();
+console.log(globalCVar);
+console.log(localCVar);
+console.log(blockCVar); {
+    
+}
---
+++ index.js
@@ -0,0 +1,59 @@
+/*let cartTotal = 0;
+cartTotal = cartTotal + 50;
+cartTotal = cartTotal + 5;
+cartTotal = cartTotal + 20;
+console.log("cartTotal is :$"+cartTotal);
+
+
+const TaxRate = 0.10;
+
+let price = 100;
+TotalPrice = price + (price * TaxRate);
+console.log("TotalPrice is :" + TotalPrice,"RWF");
+
+
+
+let userName = "thierry";
+
+function greetUser() {
+    console.log("Hello " + userName + "!");
+}
+greetUser();
+console.log("userName is :" + userName); 
+
+let myname = "thierry"
+
+console.log(myname.startsWith('t'));
+
+
+
+     
+     
+     
+let count = "musa";
+count += "musa";
+ 
+ console.log(count);
+ 
+ 
+ let names = "thierry";
+ console.log(names.toUpperCase());
+
+ 
+ 
+ symbol1 = Symbol("id1");
+ let symbol2 = Symbol("id1");
+ console.log(symbol1 === symbol2);
+ const symbol1 = Symbol("id1");
+ const symbol2 = Symbol("id2");
+    console.log(symbol1 === symbol2);
+
+ */
+ console.log(typeof "thierry");
+ console.log(typeof 10);
+ console.log(typeof true);
+ let value;
+ 
+
+
+
